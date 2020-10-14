0ae18257fd4a3ba9b05c967fb1a3f056522c5e4c
Marlon Menezes <marlusher@gmail.com>
2020-10-14 00:37:32 -0300

    Criado a página de loalização dos orfanatos disponíveis (Orphanages). Criação, estilização total e funções JavaScript

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,55 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+// Create a map
+
+const map = L.map('mapid', options).setView([-27.222633, -49.6455874], 15);
+
+//create and add tileLayer
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+
+//Create icon
+
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+//create and add marker
+
+L.marker([-27.222633, -49.6455874], {icon}).addTo(map)
+
+//Image Gallery
+
+function selectImage(event){
+    const button = event.currentTarget
+
+    // remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button){
+        button.classList.remove("active")
+    }
+
+
+    // selecionar a imagem clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    // atualizar o container da imagem
+
+    imageContainer.src = image.src
+
+    // adicionar a classe .active para este botão
+    button.classList.add('active')
+}
