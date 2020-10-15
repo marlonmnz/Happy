cc4fe5444a86696a77d4b762a5888a3a1e11561c
Marlon Menezes <marlusher@gmail.com>
2020-10-15 20:49:06 -0300

    Criação do servidor e configuração total do Back-End utilizando NODE.JS.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,25 @@
 // Importar dependências (lib)
 
 const express = require('express');
+const path = require('path');
+const pages = require('./pages.js');
 
 // iniciando o express
 const server = express()
+server
 
-// Criar uma rota
-server.get('/', () => {
-    
-})
+// Utilizando os arquivos estáticos
+.use(express.static('public'))
+
+// Configurar template engine
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
+
+// Rotas da aplicação
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
 
 // Ligar o servidor
 
